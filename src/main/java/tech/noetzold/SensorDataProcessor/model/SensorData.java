c485eafead01d4a7407406156c230b4a85aa211b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-07-29 22:59:09 -0300

    Create SensorData.java

---
+++ src/main/java/tech/noetzold/SensorDataProcessor/model/SensorData.java
@@ -0,0 +1,25 @@
+package tech.noetzold.SensorDataProcessor.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class SensorData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sensorType;
+    private double value;
+    private String coordinates;
+    private LocalDateTime timestamp;
+
+}
