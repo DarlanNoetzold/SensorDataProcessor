67e125ac3fc71d13c874a1c9e97b2e5940a0ecf7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-07-30 23:24:33 -0300

    Delete SensorDataRepository.java

--- src/main/java/tech/noetzold/SensorDataProcessor/repository/SensorDataRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.noetzold.SensorDataProcessor.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.SensorDataProcessor.model.SensorData;
-
-public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
-}
