66b0be4fe227ee45408114e447a9bbe9b7465abf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-07-31 21:09:43 -0300

    Create data_compression.c

---
+++ c/data_compression.c
@@ -0,0 +1,21 @@
+#include <jni.h>
+#include <stdio.h>
+#include <math.h>
+#include "com_example_sensordataprocessor_service_DataService.h"
+
+JNIEXPORT jdoubleArray JNICALL Java_com_example_sensordataprocessor_service_DataService_compressData
+  (JNIEnv *env, jobject obj, jdoubleArray data) {
+    jsize length = (*env)->GetArrayLength(env, data);
+    jdouble *elements = (*env)->GetDoubleArrayElements(env, data, 0);
+
+    // Compressão simples: arredondar para duas casas decimais
+    for (int i = 0; i < length; i++) {
+        elements[i] = round(elements[i] * 100.0) / 100.0;
+    }
+
+    jdoubleArray result = (*env)->NewDoubleArray(env, length);
+    (*env)->SetDoubleArrayRegion(env, result, 0, length, elements);
+
+    (*env)->ReleaseDoubleArrayElements(env, data, elements, 0);
+    return result;
+}
