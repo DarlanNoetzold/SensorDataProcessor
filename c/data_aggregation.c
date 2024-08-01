407b459c2fb2a9a13942c394a5ba8744075e72f6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-07-31 21:09:38 -0300

    Create data_aggregation.c

---
+++ c/data_aggregation.c
@@ -0,0 +1,30 @@
+#include <jni.h>
+#include <stdio.h>
+#include "com_example_sensordataprocessor_service_DataService.h"
+
+JNIEXPORT jdoubleArray JNICALL Java_com_example_sensordataprocessor_service_DataService_aggregateData
+  (JNIEnv *env, jobject obj, jdoubleArray data) {
+    jsize length = (*env)->GetArrayLength(env, data);
+    jdouble *elements = (*env)->GetDoubleArrayElements(env, data, 0);
+
+    int blockSize = 5; // Tamanho do bloco para agregação
+    int newLength = (length + blockSize - 1) / blockSize; // Tamanho do array resultante
+
+    jdouble *aggregated = malloc(newLength * sizeof(jdouble));
+    for (int i = 0; i < newLength; i++) {
+        double sum = 0.0;
+        int count = 0;
+        for (int j = 0; j < blockSize && (i * blockSize + j) < length; j++) {
+            sum += elements[i * blockSize + j];
+            count++;
+        }
+        aggregated[i] = sum / count;
+    }
+
+    jdoubleArray result = (*env)->NewDoubleArray(env, newLength);
+    (*env)->SetDoubleArrayRegion(env, result, 0, newLength, aggregated);
+
+    free(aggregated);
+    (*env)->ReleaseDoubleArrayElements(env, data, elements, 0);
+    return result;
+}
