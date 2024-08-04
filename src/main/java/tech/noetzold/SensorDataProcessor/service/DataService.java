3af624701ad15e349db7023f4785427513922254
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-08-04 10:41:26 -0300

    Update DataService.java

--- src/main/java/tech/noetzold/SensorDataProcessor/service/DataService.java
+++ src/main/java/tech/noetzold/SensorDataProcessor/service/DataService.java
@@ -1,5 +1,7 @@
 package tech.noetzold.SensorDataProcessor.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.noetzold.SensorDataProcessor.model.SensorDataProcessed;
@@ -14,6 +16,8 @@ import java.util.stream.Collectors;
 @Service
 public class DataService {
 
+    private static final Logger logger = LoggerFactory.getLogger(DataService.class);
+
     @Autowired
     private SensorDataRawRepository sensorDataRawRepository;
 
@@ -21,9 +25,14 @@ public class DataService {
     private SensorDataProcessedRepository sensorDataProcessedRepository;
 
     static {
-        System.loadLibrary("data_filter");
-        System.loadLibrary("data_compression");
-        System.loadLibrary("data_aggregation");
+        try {
+            System.loadLibrary("libdata_filter");
+            System.loadLibrary("libdata_compression");
+            System.loadLibrary("libdata_aggregation");
+            logger.info("Libraries loaded successfully.");
+        } catch (UnsatisfiedLinkError e) {
+            logger.error("Failed to load libraries.", e);
+        }
     }
 
     public native double[] filterData(double[] data);
