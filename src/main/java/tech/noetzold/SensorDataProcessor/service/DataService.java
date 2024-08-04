b4474c2ac361a5e30c8259c5a6d6dd1ef0ce49bc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-08-04 18:09:01 -0300

    Update DataService.java

--- src/main/java/tech/noetzold/SensorDataProcessor/service/DataService.java
+++ src/main/java/tech/noetzold/SensorDataProcessor/service/DataService.java
@@ -3,15 +3,16 @@ package tech.noetzold.SensorDataProcessor.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import tech.noetzold.SensorDataProcessor.model.SensorDataProcessed;
 import tech.noetzold.SensorDataProcessor.model.SensorDataRaw;
 import tech.noetzold.SensorDataProcessor.repository.SensorDataProcessedRepository;
 import tech.noetzold.SensorDataProcessor.repository.SensorDataRawRepository;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Arrays;
 
 @Service
 public class DataService {
@@ -24,6 +25,9 @@ public class DataService {
     @Autowired
     private SensorDataProcessedRepository sensorDataProcessedRepository;
 
+    @Value("${use.native.heuristics:false}")
+    private boolean useNativeHeuristics;
+
     static {
         try {
             System.loadLibrary("libdata_filter");
@@ -35,16 +39,67 @@ public class DataService {
         }
     }
 
+    // Métodos nativos
     public native double[] filterData(double[] data);
 
     public native double[] compressData(double[] data);
 
     public native double[] aggregateData(double[] data);
 
+    // Métodos Java
+    public double[] filterDataJava(double[] data) {
+        return Arrays.stream(data).distinct().filter(d -> d > 0.1).toArray();
+    }
+
+    public double[] compressDataJava(double[] data) {
+        return Arrays.stream(data).map(d -> Math.round(d * 100.0) / 100.0).toArray();
+    }
+
+    public double[] aggregateDataJava(double[] data) {
+        int blockSize = 5;
+        int newLength = (data.length + blockSize - 1) / blockSize;
+        double[] aggregated = new double[newLength];
+        for (int i = 0; i < newLength; i++) {
+            double sum = 0.0;
+            int count = 0;
+            for (int j = 0; j < blockSize && (i * blockSize + j) < data.length; j++) {
+                sum += data[i * blockSize + j];
+                count++;
+            }
+            aggregated[i] = sum / count;
+        }
+        return aggregated;
+    }
+
+    // Métodos para decidir se usa nativo ou Java
+    public double[] filterDataNativeOrJava(double[] data) {
+        if (useNativeHeuristics) {
+            return filterData(data);
+        } else {
+            return filterDataJava(data);
+        }
+    }
+
+    public double[] compressDataNativeOrJava(double[] data) {
+        if (useNativeHeuristics) {
+            return compressData(data);
+        } else {
+            return compressDataJava(data);
+        }
+    }
+
+    public double[] aggregateDataNativeOrJava(double[] data) {
+        if (useNativeHeuristics) {
+            return aggregateData(data);
+        } else {
+            return aggregateDataJava(data);
+        }
+    }
+
     public List<SensorDataRaw> dataFilter(SensorDataRaw sensorDataRaw) {
         List<SensorDataRaw> data = sensorDataRawRepository.findAll();
         double[] rawData = data.stream().mapToDouble(SensorDataRaw::getValue).toArray();
-        double[] filteredData = filterData(rawData);
+        double[] filteredData = filterDataNativeOrJava(rawData);
         return data.stream().filter(d -> {
             for (double v : filteredData) {
                 if (d.getValue() == v) {
@@ -57,7 +112,7 @@ public class DataService {
 
     public List<SensorDataRaw> dataCompression(List<SensorDataRaw> data) {
         double[] rawData = data.stream().mapToDouble(SensorDataRaw::getValue).toArray();
-        double[] compressedData = compressData(rawData);
+        double[] compressedData = compressDataNativeOrJava(rawData);
         return data.stream().filter(d -> {
             for (double v : compressedData) {
                 if (d.getValue() == v) {
@@ -70,7 +125,7 @@ public class DataService {
 
     public List<SensorDataRaw> dataAggregation(List<SensorDataRaw> data) {
         double[] rawData = data.stream().mapToDouble(SensorDataRaw::getValue).toArray();
-        double[] aggregatedData = aggregateData(rawData);
+        double[] aggregatedData = aggregateDataNativeOrJava(rawData);
         return data.stream().filter(d -> {
             for (double v : aggregatedData) {
                 if (d.getValue() == v) {
