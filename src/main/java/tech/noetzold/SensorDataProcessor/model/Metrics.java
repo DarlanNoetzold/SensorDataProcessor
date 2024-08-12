ad50a2c280bfed11c751294d998180675cc733a6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-08-12 20:49:37 -0300

    Create Metrics.java

---
+++ src/main/java/tech/noetzold/SensorDataProcessor/model/Metrics.java
@@ -0,0 +1,30 @@
+package tech.noetzold.SensorDataProcessor.model;
+
+import lombok.Data;
+
+import jakarta.persistence.*;
+import java.util.Map;
+
+@Entity
+@Data
+public class Metrics {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double cpuUsage;
+    private long memoryUsage;
+    private int threadCount;
+
+    @ElementCollection
+    @CollectionTable(name = "sensor_variance", joinColumns = @JoinColumn(name = "metrics_id"))
+    @MapKeyColumn(name = "sensor_type")
+    @Column(name = "variance")
+    private Map<String, Double> varianceMap;
+
+    private long totalDataReceived;
+    private long totalDataFiltered;
+    private long totalDataCompressed;
+    private long errorCount;
+}
