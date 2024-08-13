c68d9b1080f648cc75dd1e02b552caf16ceb6cbe
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-08-12 21:34:06 -0300

    Update SensorDataProcessorApplication.java

--- src/main/java/tech/noetzold/SensorDataProcessor/SensorDataProcessorApplication.java
+++ src/main/java/tech/noetzold/SensorDataProcessor/SensorDataProcessorApplication.java
@@ -2,8 +2,10 @@ package tech.noetzold.SensorDataProcessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SensorDataProcessorApplication {
 
 	public static void main(String[] args) {
